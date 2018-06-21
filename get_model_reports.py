f441e816ed4d45a9b0239e0309b9f0349d394b60
EvilPort <dibakarsaha1234@gmail.com>
2018-06-22 04:09:47 +0530

    Confusion matrix and other reports added

---
+++ get_model_reports.py
@@ -0,0 +1,105 @@
+from keras.models import load_model
+from sklearn.metrics import classification_report, confusion_matrix
+import pickle
+import numpy as np
+import seaborn as sn
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plot_confusion_matrix(cm,
+                          target_names,
+                          title='Confusion matrix',
+                          cmap=None,
+                          normalize=True):
+    """
+    given a sklearn confusion matrix (cm), make a nice plot
+
+    Arguments
+    ---------
+    cm:           confusion matrix from sklearn.metrics.confusion_matrix
+
+    target_names: given classification classes such as [0, 1, 2]
+                  the class names, for example: ['high', 'medium', 'low']
+
+    title:        the text to display at the top of the matrix
+
+    cmap:         the gradient of the values displayed from matplotlib.pyplot.cm
+                  see http://matplotlib.org/examples/color/colormaps_reference.html
+                  plt.get_cmap('jet') or plt.cm.Blues
+
+    normalize:    If False, plot the raw numbers
+                  If True, plot the proportions
+
+    Usage
+    -----
+    plot_confusion_matrix(cm           = cm,                  # confusion matrix created by
+                                                              # sklearn.metrics.confusion_matrix
+                          normalize    = True,                # show proportions
+                          target_names = y_labels_vals,       # list of names of the classes
+                          title        = best_estimator_name) # title of graph
+
+    Citiation
+    ---------
+    http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+
+    """
+    import itertools
+
+    accuracy = np.trace(cm) / float(np.sum(cm))
+    misclass = 1 - accuracy
+
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+
+    plt.figure(figsize=(5, 5))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+    plt.show()
+
+
+image_x, image_y = 50, 50
+with open("test_images", "rb") as f:
+	test_images = np.array(pickle.load(f))
+with open("test_labels", "rb") as f:
+	test_labels = np.array(pickle.load(f), dtype=np.int32)
+test_images = np.reshape(test_images, (test_images.shape[0], image_x, image_y, 1))
+
+
+model = load_model('cnn_model_keras2.h5')
+pred_labels = []
+pred_probabs = model.predict(test_images)
+for pred_probab in pred_probabs:
+	pred_labels.append(list(pred_probab).index(max(pred_probab)))
+
+cm = confusion_matrix(test_labels, np.array(pred_labels))
+classification_report = classification_report(test_labels, np.array(pred_labels))
+print('\n\nClassification Report') 
+print('---------------------------')
+print(classification_report)
+plot_confusion_matrix(cm, range(44), normalize=False)
