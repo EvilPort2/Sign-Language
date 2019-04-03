b6aaff5fb03ad00b2b20bb2d46b27e054f3b871d
EvilPort <dibakarsaha1234@gmail.com>
2019-04-03 18:56:33 +0530

    Update load_images.py

--- load_images.py
+++ load_images.py
@@ -4,6 +4,7 @@ import numpy as np
 import random
 from sklearn.utils import shuffle
 import pickle
+import os
 
 def pickle_images_labels():
 	images_labels = []
@@ -11,7 +12,7 @@ def pickle_images_labels():
 	images.sort()
 	for image in images:
 		print(image)
-		label = image[image.find('/')+1: image.rfind('/')]
+		label = image[image.find(os.sep)+1: image.rfind(os.sep)]
 		img = cv2.imread(image, 0)
 		images_labels.append((np.array(img, dtype=np.uint8), int(label)))
 	return images_labels
